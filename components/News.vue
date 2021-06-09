7a327e265a9e07e2e7bf63542d92f1c633dfde89
taisto-seppanen <taistopetrseppanen@gmail.com>
2021-06-09 11:06:46 +0300

    feat: news without pics

--- components/News.vue
+++ components/News.vue
@@ -4,8 +4,9 @@
           <div class='gridItem'>
             <img
             :src="news.newsPic"
+            onError="this.src='/_nuxt/assets/newspic.jpg'"
             class="card-img-top"
-            style="max-width: 400px; min-width: 300px"
+            style="max-width: 400px;"
             />
           </div>
           <div class='gridItem'>
@@ -39,17 +40,25 @@ export default {
       let defaultNews = [
         {
           id: 0,
-          newsDate: '1991-04-01',
+          newsDate: '1978-05-01',
           newsTittle: "Завершение строительсва кинотеатра",
           newsPic: "http://rk.karelia.ru/wp-content/uploads/2020/03/irewhyg8y2o0o8go04.jpg",
           newsText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         },
         {
           id: 1,
-          newsDate: '1991-04-01',
+          newsDate: '2021-05-09',
           newsTittle: "Запуск сайта",
           newsPic: "https://heworthgrange.org/wp-content/uploads/2021/04/istockphoto-1197831888-612x612-1.jpg",
           newsText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        },
+
+        {
+          id: 2,
+          newsDate: '2021-05-10',
+          newsTittle: "‘Stand By Me’ Returning to Theaters For 35th Anniversary",
+          newsPic: "https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/Stand-By-Me-Returning-to-Theaters.jpg",
+          newsText: "Stand By Me, the Rob Reiner movie based on Stephen King‘s novella “The Body,” is headed back to the big screen to mark the movie’s 35th anniversary. The screenings will feature the bonus featurette “Walking the Tracks: The Summer of Stand by Me,” which follows “Stephen King as he recalls what led him to write the stories of his childhood, how they impacted the development of the movie, and Rob Reiner as he reminisces about the casting of the four leads.” Screenings will take place in theaters nationwide on May 23 and May 26.",
         },
       ];
       localStorage.setItem("newsArray", JSON.stringify(defaultNews));
