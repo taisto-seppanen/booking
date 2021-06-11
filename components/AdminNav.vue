6f9cf463ddbc2a356e1fb5b4397f3ab55554639f
taisto-seppanen <taistopetrseppanen@gmail.com>
2021-06-11 11:48:28 +0300

    feat: add contacts edit page

--- components/AdminNav.vue
+++ components/AdminNav.vue
@@ -2,13 +2,14 @@
 
 <div>
   <b-navbar toggleable="lg" type="dark" variant="dark">
-    <b-navbar-brand href="#">Админ</b-navbar-brand>
+    <b-navbar-brand href="#">Админка</b-navbar-brand>
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
         <NuxtLink to="../admin/" class="nav-link" no-prefetch>Главная</NuxtLink>
-        <NuxtLink to="../admin/NewsPandel" class="nav-link" no-prefetch>Редактирование новостей</NuxtLink>
-        <NuxtLink to='../admin/FilmsPanel' no-prefetch class="nav-link">Редактирование фильмов</NuxtLink>
+        <NuxtLink to="../admin/NewsPandel" class="nav-link" no-prefetch>Новости</NuxtLink>
+        <NuxtLink to='../admin/FilmsPanel' no-prefetch class="nav-link">Фильмы</NuxtLink>
+        <NuxtLink to='../admin/contacts' no-prefetch class="nav-link">Контакты</NuxtLink>
         <a to='href' @click="logout" no-prefetch class="nav-link">Выйти</a>
       </b-navbar-nav>
     </b-collapse>
---
+++ pages/admin/contacts.vue
@@ -0,0 +1,95 @@
+<template>
+    <b-container md="4">
+    <b-form @submit.stop.prevent='save'>
+        <b-form-group class="m-3"
+            id="input-group-1"
+            label="Адрес:"
+            label-for="input-1"
+            description="город, улица, дом, индекс">
+                <b-form-input id="input-1" type="text" v-model="contacts.adress"
+                placeholder="Адрес" required></b-form-input>
+        </b-form-group>
+
+        <b-form-group class="m-3"
+            id="input-group-1"
+            label="Телефонный номер:"
+            label-for="input-1"
+            description="в формате 8(8412)757-757">
+                <b-form-input id="input-1" type="text" v-model="contacts.telephone"
+                placeholder="телефон" required></b-form-input>
+        </b-form-group>
+
+        <b-form-group class="m-3"
+            id="input-group-1"
+            label="Ссылка на вк:"
+            label-for="input-1"
+            description="Например https://vk.com/lololo">
+                <b-form-input id="input-1" type="text" v-model="contacts.vkLink"
+                placeholder="ссылка" required></b-form-input>
+        </b-form-group>
+
+        <b-form-group class="m-3"
+            id="input-group-1"
+            label="Ссылка на инстаграм:"
+            label-for="input-1"
+            description="Например https://www.instagram.com/lindemannofficial/">
+                <b-form-input id="input-1" type="text" v-model='contacts.instagramLink'
+                placeholder="ссылка" ></b-form-input>
+        </b-form-group>
+
+        <b-form-group class="m-3"
+            id="input-group-1"
+            label="Ссылка на телеграмм:"
+            label-for="input-1"
+            description="Например https://t.me/taisto_seppanen">
+                <b-form-input id="input-1" type="text" v-model='contacts.telegramLink'
+                placeholder="ссылка" ></b-form-input>
+        </b-form-group>     
+
+        <b-button type="submit" variant="primary">Save</b-button>
+
+    </b-form>
+    </b-container>
+</template>
+
+<script>
+export default {
+    layout: "admin",
+
+    data() {
+        return {
+            contacts: {},
+        };
+    },
+
+    mounted() {
+        if (localStorage.authorization == "true") {            
+        } else {
+        console.log(localStorage.authorization == "true")
+        this.$router.push('../login/');
+        }
+        
+        if (!localStorage.contacts) this.setDefaultValues();
+        if (localStorage.contacts) {
+            this.contacts = JSON.parse(localStorage.getItem("contacts"));
+        }
+    },
+
+methods: {
+    save () {
+    localStorage.setItem("contacts", JSON.stringify(this.contacts));
+    },
+
+    async setDefaultValues() {
+        let defaultContacts = { 
+            adress : "Первомайский проспект 2 Петрозаводск Карелия Републиц, Петрозаводск, Респ. Карелия, 185001",
+            telephone : "8(8142)76-55-70",
+            vkLink : "https://vk.com/ptzkino",
+            instagramLink : "",
+            telegramLink : "",
+            };
+        localStorage.setItem("contacts", JSON.stringify(defaultContacts));
+        },
+    },
+}
+</script>
